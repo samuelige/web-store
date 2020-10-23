be665c1ead993c9d7d2f706bab6f2f7abf85671b
samuelige <samuelige.iyanu@gmail.com>
2020-10-23 18:30:19 +0100

    Added Directories

---
+++ src/components/main/directory/MenDirectory.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+
+const directory = () => {
+    const [state, setstate] = useState({
+        sections: [{
+            title: 'Hats',
+            imageURL: 'https://www.thetrendspotter.net/best-mens-hat-styles-need-know/mens-hat-styles/',
+            id: uuidv4()
+        },
+        {
+            title: 'Jackets',
+            imageURL: 'https://canary.contestimg.wish.com/api/webimage/5bebcb1b0132526ffae7ede1-large.jpg?cache_buster=32858f456ee63a351896c76d059ff966',
+            id: uuidv4()
+        }
+    ]
+    })
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default directory
